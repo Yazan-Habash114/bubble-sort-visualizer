0f79780c17d78609ac3b51387d4bd05cb404d6bc
Yazan-Habash114 <54286064+Yazan-Habash114@users.noreply.github.com>
2023-04-03 12:14:11 +0300

    Make the code prettier using extension

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Bubble sort algorithm visualizer
+
 HTML, CSS and JavaScript project to visualize the bubble sort algorithm, you can set the array size and animation speed via a slider then start sorting.
 
 ## Technologies Used
+
 <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer">
   <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/>
 </a>
@@ -13,7 +15,9 @@ HTML, CSS and JavaScript project to visualize the bubble sort algorithm, you can
 </a>
 
 ## GitHub page link
+
 You can visit the deployed project using GitHub pages from [here](https://yazan-habash114.github.io/bubble-sort-visualizer/)
 
 ## Author
+
 [Yazan Habash](https://github.com/Yazan-Habash114)
--- script.js
+++ script.js
@@ -1,129 +1,124 @@
-const label_size = document.getElementById('size');
-const label_speed = document.getElementById('speed');
-const size_slider = document.getElementById('array-size');
-const animation_speed_slider = document.getElementById('animation-speed');
-const array_elements = document.querySelector('section');
-const random_button = document.getElementById('random-button');
-const solve_button = document.getElementById('solve-button');
+const label_size = document.getElementById("size");
+const label_speed = document.getElementById("speed");
+const size_slider = document.getElementById("array-size");
+const animation_speed_slider = document.getElementById("animation-speed");
+const array_elements = document.querySelector("section");
+const random_button = document.getElementById("random-button");
+const solve_button = document.getElementById("solve-button");
 
 var size = size_slider.value;
 var speed = 800;
 
-
 const speed_dict = {
-    "1": 650,
-    "2": 500,
-    "3": 350,
-    "4": 200,
-    "5": 50
+  1: 650,
+  2: 500,
+  3: 350,
+  4: 200,
+  5: 50,
 };
 
+size_slider.addEventListener("change", () => {
+  size = size_slider.value;
+  label_size.innerHTML = `Size: ${size}/100`;
+});
 
-size_slider.addEventListener('change', () => {
-    size = size_slider.value;
-    label_size.innerHTML = `Size: ${size}/100`;
-})
+animation_speed_slider.addEventListener("change", () => {
+  value = animation_speed_slider.value;
+  speed = speed_dict[value];
+  label_speed.innerHTML = `Speed: ${value}/5`;
+});
 
-animation_speed_slider.addEventListener('change', () => {
-    value = animation_speed_slider.value;
-    speed = speed_dict[value];
-    label_speed.innerHTML = `Speed: ${value}/5`;
-})
-
-solve_button.addEventListener('click', bubble_sort);
-random_button.addEventListener('click', randomize_array);
-
+solve_button.addEventListener("click", bubble_sort);
+random_button.addEventListener("click", randomize_array);
 
 let get_random_number = () => Math.ceil(Math.random() * 100);
 
 function clear_blocks() {
-    while (array_elements.firstChild)
-        array_elements.removeChild(array_elements.lastChild);
+  while (array_elements.firstChild)
+    array_elements.removeChild(array_elements.lastChild);
 }
 
 function create_block(random, i) {
-    let block = document.createElement('div');
-    let blockWidth = (window.innerWidth / size) - 2;
-    block.classList.add('block');
-    block.style.height = `${random * 3}px`;
-    block.style.width = `${blockWidth}px`;
-    block.style.transform = `translate(${i * (blockWidth + 2)}px)`;
-    return block;
+  let block = document.createElement("div");
+  let blockWidth = window.innerWidth / size - 2;
+  block.classList.add("block");
+  block.style.height = `${random * 3}px`;
+  block.style.width = `${blockWidth}px`;
+  block.style.transform = `translate(${i * (blockWidth + 2)}px)`;
+  return block;
 }
 
 function create_block_label(random) {
-    let label = document.createElement('p');
-    label.classList.add('block_id');
-    label.innerHTML = random;
-    return label;
+  let label = document.createElement("p");
+  label.classList.add("block_id");
+  label.innerHTML = random;
+  return label;
 }
 
 function randomize_array() {
-    clear_blocks();
-    for (let i = 0; i < size; i += 1) {
-        let random_value = get_random_number();
-        let block = create_block(random_value, i);
-        let bar_label = create_block_label(random_value);
-        block.appendChild(bar_label);
-        array_elements.appendChild(block);
-    }
+  clear_blocks();
+  for (let i = 0; i < size; i += 1) {
+    let random_value = get_random_number();
+    let block = create_block(random_value, i);
+    let bar_label = create_block_label(random_value);
+    block.appendChild(bar_label);
+    array_elements.appendChild(block);
+  }
 }
 
 function swap(el1, el2) {
-    return new Promise(resolve => {
-        let temp = el1.style.transform;
-        el1.style.transform = el2.style.transform;
-        el2.style.transform = temp;
+  return new Promise((resolve) => {
+    let temp = el1.style.transform;
+    el1.style.transform = el2.style.transform;
+    el2.style.transform = temp;
 
-        window.requestAnimationFrame(() => {
-            // Wait for milliseconds between movements according to speed_dict
-            setTimeout(() => {
-                array_elements.insertBefore(el2, el1);
-                resolve();
-            }, speed);
-        })
-    })
+    window.requestAnimationFrame(() => {
+      // Wait for milliseconds between movements according to speed_dict
+      setTimeout(() => {
+        array_elements.insertBefore(el2, el1);
+        resolve();
+      }, speed);
+    });
+  });
 }
 
 async function delay(ms) {
-    await new Promise(resolve =>
-        setTimeout(() => resolve(), ms)
-    );
+  await new Promise((resolve) => setTimeout(() => resolve(), ms));
 }
 
 async function colorize_block(block, color) {
-    block.style.backgroundColor = color;
+  block.style.backgroundColor = color;
 }
 
 async function disable_solve_button() {
-    solve_button.disabled = true;
+  solve_button.disabled = true;
 }
 
 async function enable_solve_button() {
-    solve_button.disabled = false;
+  solve_button.disabled = false;
 }
 
 async function bubble_sort() {
-    let blocks = document.querySelectorAll(".block");
-    disable_solve_button();
-    for (let i = 0; i < blocks.length; i += 1) {
-        for (let j = 0; j < blocks.length - i - 1; j += 1) {
-            colorize_block(blocks[j], "#feca57");
-            colorize_block(blocks[j + 1], "#feca57");
+  let blocks = document.querySelectorAll(".block");
+  disable_solve_button();
+  for (let i = 0; i < blocks.length; i += 1) {
+    for (let j = 0; j < blocks.length - i - 1; j += 1) {
+      colorize_block(blocks[j], "#feca57");
+      colorize_block(blocks[j + 1], "#feca57");
 
-            delay(150);     // Delay between comparisons
+      delay(150); // Delay between comparisons
 
-            let value1 = Number(blocks[j].childNodes[0].innerHTML);
-            let value2 = Number(blocks[j + 1].childNodes[0].innerHTML);
-            if (value1 > value2) {
-                await swap(blocks[j], blocks[j + 1]);
-                blocks = document.querySelectorAll(".block");
-            }
-            colorize_block(blocks[j], "#f368e0");
-            colorize_block(blocks[j + 1], "#f368e0");
-        }
-        // Colorize the sorted block
-        colorize_block(blocks[blocks.length - i - 1], "#222f3e");
+      let value1 = Number(blocks[j].childNodes[0].innerHTML);
+      let value2 = Number(blocks[j + 1].childNodes[0].innerHTML);
+      if (value1 > value2) {
+        await swap(blocks[j], blocks[j + 1]);
+        blocks = document.querySelectorAll(".block");
+      }
+      colorize_block(blocks[j], "#f368e0");
+      colorize_block(blocks[j + 1], "#f368e0");
     }
-    enable_solve_button();
+    // Colorize the sorted block
+    colorize_block(blocks[blocks.length - i - 1], "#222f3e");
+  }
+  enable_solve_button();
 }
